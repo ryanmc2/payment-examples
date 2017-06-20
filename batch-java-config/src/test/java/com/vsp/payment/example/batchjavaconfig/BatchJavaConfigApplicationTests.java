a8f5082c3f3e00416d006c0ac169ed7d9a32d4bc
Ryan McCullough <ryanmc2@vsp.com>
2017-06-20 14:07:08 -0700

    initial commit

---
+++ batch-java-config/src/test/java/com/vsp/payment/example/batchjavaconfig/BatchJavaConfigApplicationTests.java
@@ -0,0 +1,16 @@
+package com.vsp.payment.example.batchjavaconfig;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BatchJavaConfigApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
